d5bf2ef1ea3688185b2b86da7844cffe1d6205f1
Zhenya <deadrime@yandex.ru>
2017-08-03 21:30:05 +0300

    modified:   yandex-form/index.html 	modified:   yandex-form/index.js 	deleted:    yandex-form/package-lock.json 	deleted:    yandex-form/projects 	renamed:    yandex-form/error.json -> yandex-form/server-response/error.json 	renamed:    yandex-form/progress.json -> yandex-form/server-response/progress.json 	renamed:    yandex-form/success.json -> yandex-form/server-response/success.json 	deleted:    yandex-form/style.css 	new file:   yandex-form/styles/main.css 	new file:   yandex-form/styles/main.styl 	new file:   yandex-form/styles/normalize.css

--- yandex-form/index.js
+++ yandex-form/index.js
@@ -19,61 +19,65 @@ const MyForm = {
 		let errorFields = [];
 		
 		//fio
-		let reg = /[^a-zA-Zа-яА-Я\ \-]/i //tolko bukvi, probel i tire
+		let reg = /[^a-zA-Zа-яА-Я\ \-]/i // разрешены только буквы, пробелы и тире
 		let words = data['fio'].split(' ')
-		let rightAmount = words.every((word) => word.length>0); //hotya bi 1 bukba
-		let wordCount =  words.length;  // na 3 slova
-		if (wordCount !==3 || reg.test(data['fio']) || !rightAmount) {
+		let rightAmount = words.every((word) => word.length>0); // Хотя бы одна буква в слове
+		let wordCount =  words.length;  // количество слов
+		if (wordCount !==3 || reg.test(data['fio']) || !rightAmount) { 
 			isValid = false;
 			errorFields.push('input-fio');
 		}
 		
 		//email
-		reg = /.+@.+\..+/i; // html5 umeet proveryat' email, ne uveren chto eto nuzhno
-		let isEmail = data['email'].search(reg);
+		reg = /.+@.+\..+/i; // html5 умеет валидировать email, так что я не уверен, что эта проверка тут уместна
+		let BadEmail = data['email'].search(reg);
 		let mailType = data['email'].split('@')[1];
-		if (isEmail || !yandexMails.includes(mailType)) {//esli email ili poshta ne ot yandex
+		if (BadEmail || !yandexMails.includes(mailType)) { // не является почтовым адресом или не от яндекс
 			isValid = false;
 			errorFields.push('input-email');
 		}
 			
-		//phone
-		reg = /\d/g;  //tak kak telephon rabotaet po maske - prosto nahozhu vse cifri
+		//телефон
+		reg = /\d/g; //так как телефон работает по маске - просто нахожу все цифры
 		let summ = 0;
 		let nums = data['phone'].match(reg);
-		nums.map(num => summ+=parseInt(num)); //summa chisel	
-		if (summ >30 || nums.length!== 11) {
+		nums.map(num => summ+=parseInt(num)); //и считаю их сумму
+		if (summ >30 || nums.length!== 11) { // неправильная сумма или длина номера
 			isValid = false;
 			errorFields.push('input-phone');
 		}
-		
 		return {isValid, errorFields};
 	},
 	sendData(data) {
 		let xhr = new XMLHttpRequest();
-		let urlParams = Object.keys(data).map(i => i+'='+ encodeURIComponent(data[i])).join('&');
+		let urlParams = Object.keys(data).map(i => i+'='+ encodeURIComponent(data[i])).join('&'); 
 		//console.log(urlParams);
-		xhr.open('GET', serverAddr + '?'+urlParams, true);
+		xhr.open('GET', serverAddr + '?'+urlParams, true); // наверное это излишне, но будь сервер настоящим - нужно было бы передать данные с формы
 		xhr.setRequestHeader('Content-Type','application/json');
 		xhr.send();
 		xhr.onload = () => {
-			if(xhr.status ==200) {
+			if(xhr.status === 200) {
 				let res = JSON.parse(xhr.responseText);
 				let resultContainer = document.getElementById('resultContainer');
-				let dataFromServer = document.getElementById('dataFromServer')
+				let dataFromServer = document.getElementById('dataFromServer');
+				let submitBtn = document.getElementById('submitBtn');
 				
 				switch(res.status) {
 					case 'success':
 						resultContainer.className = 'success';
+						submitBtn.className = 'btn btn--success';
 						dataFromServer.innerHTML = xhr.responseText;
 						break;
 					case 'progress':
+						var now = new Date();
 						resultContainer.className = 'progress';
-						dataFromServer.innerHTML = xhr.responseText;
+						submitBtn.className = 'btn btn--progress';
+						dataFromServer.innerHTML = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + ' '  + xhr.responseText;
 						setTimeout(() => this.sendData(data),parseInt(res.timeout));
 						break;
 					case 'error':
 						resultContainer.className = 'error';
+						submitBtn.className = 'btn btn--error';
 						dataFromServer.innerHTML = xhr.responseText;
 						break;
 				}
@@ -81,15 +85,12 @@ const MyForm = {
 		}
 	},
 	submit() {
-		let data = this.getData();
-				
-		let isValid, errorFields;
+		let data = this.getData();		
 		let valRes = this.validate(data);
-		isValid = valRes['isValid'];
-		errorFields = valRes['errorFields'];
+		let isValid = valRes['isValid'], errorFields= valRes['errorFields'];
 			
-		if (!isValid) {
-			Array.from(form).map(i => {
+		if (!isValid) {  
+			Array.from(form).map(i => { // расставление классов
 				if (errorFields.includes(i.id)) {
 					i.classList.add('error');
 				}
@@ -99,17 +100,18 @@ const MyForm = {
 			});
 		}
 		else {
+			Array.from(form).map(i => i.classList.remove('error'));
 			let dataToServer = document.getElementById('dataToServer');
 			let resultStr = JSON.stringify(data);
 			dataToServer.innerHTML = resultStr;
 			this.sendData(data);
 		}
 	},	
-	autocompleteEmail() {
+	autocompleteEmail() { // подумал что ошибок будет меньше если помочь пользователю выбрать почтовый домен
 		let datalist = document.getElementById('autocmpl');
 		let listItems = datalist.getElementsByTagName('option');
 		let strArr = emailInput.value.split("@");
-		if (strArr.length == 2) {
+		if (strArr.length === 2) {
 			Array.from(listItems).map((item,i) => {
 				item.removeAttribute('disabled');
 				item.value = strArr[0] + '@' + yandexMails[i];
@@ -118,18 +120,16 @@ const MyForm = {
 	}
 }
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	MyForm.submit();
-})
-
 let emailInput = form['input-email'];
 emailInput.addEventListener('input', MyForm.autocompleteEmail);
 responceType.addEventListener('change', () => serverAddr = responceType.value);
-
-// maska ispolkzuya formatted.js
-let formatted = new Formatter(document.getElementById('input-phone'), {
-  'pattern': '+7 ({{999}})-{{999}}-{{99}}-{{99}}',
-  'persistent': true
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	MyForm.submit();
 });
 
+// маска телефона, используя formatted.js
+let formatted = new Formatter(document.getElementById('input-phone'), {
+  'pattern': '+7({{999}}){{999}}-{{99}}-{{99}}',
+  'persistent': true
+});
