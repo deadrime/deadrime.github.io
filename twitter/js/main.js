c888203fe95efb31691f3164ed87b6db269ddadc
Eugene <deadrime@yandex.ru>
2024-02-14 03:30:45 +0300

    Remove old projects

--- twitter/js/main.js
+++
@@ -1,7 +0,0 @@
-var menu = document.getElementsByClassName('top-nav__menu')[0];
-
-var btn = document.getElementsByClassName('top-nav__mobile-btn')[0];
-
-btn.addEventListener('click', function () {
-    menu.classList.toggle('top-nav__menu--active')
-});
