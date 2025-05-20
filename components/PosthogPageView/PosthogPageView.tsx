445cd701904b97e5458217ecff2cfa0c439e74b2
deadrime <deadrime@yandex.ru>
2025-05-21 01:11:59 +0300

    lint fix

--- components/PosthogPageView/PosthogPageView.tsx
+++ components/PosthogPageView/PosthogPageView.tsx
@@ -1,28 +1,28 @@
 // app/PostHogPageView.jsx
-'use client'
+'use client';
 
-import { usePathname, useSearchParams } from "next/navigation"
-import { useEffect, Suspense } from "react"
-import { usePostHog } from 'posthog-js/react'
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, Suspense } from "react";
+import { usePostHog } from 'posthog-js/react';
 
 function PostHogPageView() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthog = usePostHog()
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
 
   // Track pageviews
   useEffect(() => {
     if (pathname && posthog) {
-      let url = window.origin + pathname
+      let url = window.origin + pathname;
       if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`
+        url = url + `?${searchParams.toString()}`;
       }
 
-      posthog.capture('$pageview', { '$current_url': url })
+      posthog.capture('$pageview', { '$current_url': url });
     }
-  }, [pathname, searchParams, posthog])
+  }, [pathname, searchParams, posthog]);
   
-  return null
+  return null;
 }
 
 // Wrap this in Suspense to avoid the `useSearchParams` usage above
@@ -31,5 +31,5 @@ function PostHogPageView() {
 export default function SuspendedPostHogPageView() {
   return <Suspense fallback={null}>
     <PostHogPageView />
-  </Suspense>
+  </Suspense>;
 }
