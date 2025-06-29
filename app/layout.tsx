240b70e7483fe1befdf6d457cb0dbb6c01b658db
Eugene <deadrime@yandex.ru>
2025-06-29 15:00:37 +0300

    fix lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ru" suppressHydrationWarning>
       <body
         className={classNames(
           primaryFont.variable,
